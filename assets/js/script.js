a3c3faed165543b395f625f4ad9cc44b537e3a46
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-20 11:15:40 +0100

    initial copy commit

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+//L'utente clicca su un bottone che genererà  una griglia di gioco quadrata. Ogni cella ha un numero progressivo da 1 a 100. Ci saranno quindi 10 caselle x 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette in console un messaggio con il numero della cella cliccata.
+
+//identifico il bottone
+const playBtn = document.getElementById('playbtn')
+console.log(playBtn);
+
+//funzione per definire la difficoltà e le caselle relative ad essa
+function setDifficulty(value) {
+    if (value == 1) {
+        return 49
+    } else if (value == 2) {
+        return 81
+    } else {
+        return 100
+    }
+}
+
+//creiamo la funzione onclick
+function game() {
+    //identifichiamo il form-select e prendiamo il valore che inserisce l'utente
+    let difficultyInput = document.getElementById('difficulty').value
+    const boxSize = setDifficulty(difficultyInput);
+    //indentifico il container dalla dom
+    const container = document.querySelector('.container')
+    container.innerHTML = ''
+    for (let i = 1; i <= boxSize; i++) {
+        //impostiamo le box da appendere
+        const boxCreate = `<div class="box"> ${i} </div>`
+        //inseriamo le box nel container
+        container.innerHTML += boxCreate 
+    }
+}
+
+function boxColor() {
+    const box = document.querySelectorAll('.box')
+    console.log(box);
+    for (let i = 0; i < box.length; i++) {
+        const thisBox = box[i];
+        const thisBoxValue = thisBox.innerHTML
+        thisBox.addEventListener('click', function() {
+            thisBox.classList.toggle('active')
+            console.log(thisBoxValue);
+        }
+        )
+    }
+}
+
+playBtn.addEventListener('click', function() {
+    game()
+    boxColor()
+}    
+)
+
+
+
